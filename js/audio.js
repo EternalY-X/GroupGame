6ea633ea73aa28ba25b253e089754a456a24f5c6
EternalY <omazur19@gmail.com>
2026-04-14 15:01:11 +0100

    Playlists
    Added playlist placeholders

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,15 @@
-// audio.js — Playlist logic (Oiver's part)
+// audio.js — Playlist logic
+
+var playlists = 
+{
+    DollHouse: ["1-requiem-lacrimosa-mozart.mp3", "2-lullaby-brahms.mp3", "3-badinerie-bach.mp3", "4-beethoven-for-elise.mp3"],
+    Garden: [],
+    Ballroom: []
+}; // need more songs
+
+var names = 
+{
+    DollHouse: ["placeholderName1", "placeholderName2", "placeholderName3", "placeholderName4"],
+    Garden: [],
+    Ballroom: []
+};
