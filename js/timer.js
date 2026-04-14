ee0e0a166205bf166ab10347a26a8a9c8b52982f
EternalY <omazur19@gmail.com>
2026-04-14 13:13:53 +0100

    Display & Start
    Added a start timer function & time display updates

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,36 @@
-// timer.js — Pomodoro timer (Olver's module)
+// timer.js — Pomodoro timer
+
+let timeLeft = 0.5 * 60;
+let timer;
+
+
+// display time in MM/SS
+function displayTime()
+{
+    let mins = Math.floor(timeLeft/60);
+    let secs = timeLeft%60;
+
+    document.getElementById("timer-display").textContent = `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`
+}
+
+function startTimer() 
+{
+  if (timer) return;
+
+  timer = setInterval(function()
+    {
+        if (timeLeft > 0) 
+        {
+            timeLeft--;
+            displayTime();
+        } 
+        
+        else 
+        {
+        clearInterval(timer);
+        timer = null;
+        alert("Time's up!");
+        }
+
+    }, 1000);
+}
