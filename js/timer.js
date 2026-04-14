d963c916d7029d8ec35e8d24d9a5664531778ae9
EternalY <omazur19@gmail.com>
2026-04-14 13:18:57 +0100

    Countdown
    Added a countdown function to reduce code and improve clarity

--- js/timer.js
+++ js/timer.js
@@ -7,30 +7,31 @@ let timer;
 // display time in MM/SS
 function displayTime()
 {
-    let mins = Math.floor(timeLeft/60);
-    let secs = timeLeft%60;
+    var mins = Math.floor(timeLeft/60);
+    var secs = timeLeft%60;
 
     document.getElementById("timer-display").textContent = `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`
+}
+
+function countdown()
+{
+    if (timeLeft > 0) 
+    {
+        timeLeft--;
+        displayTime();
+    } 
+    
+    else 
+    {
+    clearInterval(timer);
+    timer = null;
+    alert("Time's up!"); // UP FOR CHANGE
+    }
 }
 
 function startTimer() 
 {
   if (timer) return;
 
-  timer = setInterval(function()
-    {
-        if (timeLeft > 0) 
-        {
-            timeLeft--;
-            displayTime();
-        } 
-        
-        else 
-        {
-        clearInterval(timer);
-        timer = null;
-        alert("Time's up!");
-        }
-
-    }, 1000);
+  timer = setInterval(countdown, 1000)
 }
