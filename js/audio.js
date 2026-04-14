7eab00015bd32e97a0a4acb3dfc0f03b023886cb
EternalY <omazur19@gmail.com>
2026-04-14 16:45:29 +0100

    Additional controls
    Added volume change, next/prev song and changing playlist. Also fixed up some code

--- js/audio.js
+++ js/audio.js
@@ -2,14 +2,14 @@
 
 var playlists = 
 {
-    DollHouse: ["1-requiem-lacrimosa-mozart.mp3", "2-lullaby-brahms.mp3", "3-badinerie-bach.mp3", "4-beethoven-for-elise.mp3"],
+    DollHouse: ["assets/audio/doll-house/1-requiem-lacrimosa-mozart.mp3", "assets/audio/doll-house/2-lullaby-brahms.mp3", "assets/audio/doll-house/3-badinerie-bach.mp3", "assets/audio/doll-house/4-beethoven-for-elise.mp3"],
     Garden: [],
     Ballroom: []
 }; // need more songs
 
 var names = 
 {
-    DollHouse: ["placeholderName1", "placeholderName2", "placeholderName3", "placeholderName4"],
+    DollHouse: ["ph1", "ph2", "ph3", "ph4"],
     Garden: [],
     Ballroom: []
 };
@@ -23,7 +23,7 @@ var isPlaying = false;
 
 function displaySong()
 {
-    document.getElementsByClassName("track-title").textContent = names[currentSong];
+    document.getElementsByClassName("track-title")[0].textContent = names[currentPlaylist][currentSong];
 }
 
 function togglePlay()
@@ -45,4 +45,73 @@ function togglePlay()
         <polygon points="6 3 20 12 6 21"/>
         </svg>`;
     }
+
+    displaySong();
 }
+
+function nextSong()
+{
+    music.pause();
+
+    currentSong = currentSong + 1;
+
+    if (currentSong >= playlists[currentPlaylist].length)
+    {
+        currentSong = 0;
+    }
+
+    music = new Audio(playlists[currentPlaylist][currentSong]);
+    music.volume = document.getElementById("volume").value / 100;
+    music.play();
+    isPlaying = true;
+
+    document.getElementById("toggle-play-icon").innerHTML = `pause`; // insert svg
+    displaySong();
+}
+
+function prevSong()
+{
+    music.pause();
+
+    currentSong = currentSong - 1;
+
+    if (currentSong < 0)
+    {
+        currentSong = playlists[currentPlaylist].length - 1;
+    }
+
+    music = new Audio(playlists[currentPlaylist][currentSong]);
+    music.volume = document.getElementById("volume").value / 100;
+    music.play();
+    isPlaying = true;
+
+    document.getElementById("toggle-play-icon").innerHTML = `pause`; // insert svg
+    displaySong();
+}
+
+function changePlaylist(chosenPlaylist)
+{
+    music.pause();
+
+    currentPlaylist = chosenPlaylist;
+    currentSong = 0;
+
+    music = new Audio(playlists[currentPlaylist][currentSong]);
+    music.volume = document.getElementById("volume").value / 100;
+
+    isPlaying = false;
+
+    document.getElementById("toggle-play-icon").innerHTML = 
+    `<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+    <polygon points="6 3 20 12 6 21"></polygon>
+    </svg>`;
+
+    displaySong();
+}
+
+function changeVolume()
+{
+    music.volume = document.getElementById("volume").value / 100;
+}
+
+displaySong();
