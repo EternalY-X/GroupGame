77589c42c6dc0af8df5c9560ebf41ff3f0adc99c
EternalY <omazur19@gmail.com>
2026-04-16 22:53:42 +0100

    Songs & Artists
    Updated the song and artist list

--- js/audio.js
+++ js/audio.js
@@ -7,12 +7,19 @@ var playlists =
     Ballroom: ["assets/audio/ballroom/1-gnossienne.mp3", "assets/audio/ballroom/2-toccata-and-fugue.mp3", "assets/audio/ballroom/3-297-winter.mp3", "assets/audio/ballroom/4-cello-suite-1-bach.mp3", "assets/audio/ballroom/5-moonlight-sonata.mp3"]
 };
 
-var names =
+var songs =
 {
-    DollHouse: ["Mozart – Requiem in D minor: Lacrimosa, K. 626", "Brahms – Lullaby / Wiegenlied, Op. 49, No. 4", "Bach – Badinerie, Suite No. 2 in B minor, BWV 1067", "Beethoven – Für Elise, WoO 59"],
-    Garden: ["Chopin – Nocturne in E-flat major, Op. 9, No. 2", "Debussy – Clair de Lune, Suite bergamasque, L. 75", "Beethoven – Piano Sonata No. 14 \"Moonlight\", Op. 27, No. 2", "Satie – Gymnopédie No. 1, from Trois Gymnopédies"],
-    Ballroom: ["Satie – Gymnopédie No. 1", "Bach – Toccata and Fugue in D minor, BWV 565", "Vivaldi – The Four Seasons: Winter, RV 297", "Bach – Cello Suite No. 1 in G major, BWV 1007", "Beethoven – Piano Sonata No. 14 \"Moonlight\", Op. 27, No. 2"]
+    DollHouse: ["Requiem in D minor: Lacrimosa, K. 626", "Lullaby / Wiegenlied, Op. 49, No. 4", "Badinerie, Suite No. 2 in B minor, BWV 1067", "Für Elise, WoO 59"],
+    Garden: ["Nocturne in E-flat major, Op. 9, No. 2", "Clair de Lune, Suite bergamasque, L. 75", "Piano Sonata No. 14 \"Moonlight\", Op. 27, No. 2", "Gymnopédie No. 1, from Trois Gymnopédies"],
+    Ballroom: ["Gymnopédie No. 1", "Toccata and Fugue in D minor, BWV 565", "The Four Seasons: Winter, RV 297", "Cello Suite No. 1 in G major, BWV 1007", "Piano Sonata No. 14 \"Moonlight\", Op. 27, No. 2"]
 };
+
+var artists = 
+{
+    DollHouse: ["Mozart", "Brahms", "Bach", "Beethoven"],
+    Garden: ["Chopin", "Debussy", "Beethoven", "Satie"],
+    Ballroom: ["Satie", "Bach", "Vivaldi", "Bach", "Beethoven"]
+}
 
 var currentPlaylist = "DollHouse";
 var currentSong = 0;
@@ -23,7 +30,8 @@ var isPlaying = false;
 
 function displaySong()
 {
-    document.getElementsByClassName("track-title")[0].textContent = names[currentPlaylist][currentSong];
+    document.getElementsByClassName("track-title")[0].textContent = songs[currentPlaylist][currentSong];
+    document.getElementsByClassName("track-artist")[0].textContent = artists[currentPlaylist][currentSong];
 }
 
 function togglePlay()
